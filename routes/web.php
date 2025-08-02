1161890375de5b3f8042b1a36e2301005ec5d9bb
Tio Haidar <atiohaidar@gmail.com>
2025-08-02 14:50:45 +0700

    support frontend

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiDocumentationController;
 
 
-Route::get('/', [ApiDocumentationController::class, 'index']);
+Route::get('/api/doc', [ApiDocumentationController::class, 'index']);
 
+// Fallback hanya untuk SPA route, bukan asset
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '^(?!assets|favicon\\.ico|robots\\.txt|vite\\.svg).*$');
 
 // Route::get('/', function () {
 //     return view('welcome');
